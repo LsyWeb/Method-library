45a11454e1cac57d35d79773debd0103b65f4e46
Liu Shuaiyang <83193795+LsyWeb@users.noreply.github.com>
2022-12-29 06:24:58 +0000

    add 生成随机颜色

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@
 const randomString = () => {
     return Math.random().toString(36).slice(2)
 }
+
+/**
+ * 生成随机颜色
+ */
+const randomColor = () => {
+    return '#' + Math.floor(Math.random() * 0xffffff).toString(16).padEnd(6, '0')
+}
